22039e001e03c321ea9bbc4e6641787f06a9c55f
Johnatan Santos <jonystrit@gmail.com>
2022-02-15 17:28:27 -0300

    create TO-DO-LIST (JS VANILLA)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let buttonAdd = $("#buttonAdd").click(()=>{
+/* let buttonAdd = $("#buttonAdd").click(()=>{
     let newItem = $("#inputItem").val()
     if(newItem == ''){
         alert('Por favor, digite algo')        
@@ -10,4 +10,44 @@ let buttonAdd = $("#buttonAdd").click(()=>{
 })
 let buttonClear = $("#buttonClear").click(()=>{
     $("#lista1").empty()
+}) */
+let items = []
+
+
+function listItem(){
+    items.map((item)=>{
+    const lista = document.querySelector("#lista")
+    const listItem = document.createElement('li')
+
+    const remove = document.createElement('input')
+    remove.type = "button"
+    remove.value = "X"
+    remove.id = "removeButton"
+
+    listItem.innerText = item
+    listItem.appendChild(remove)
+    lista.appendChild(listItem)
+
 })
+}
+
+
+const buttonAdd = document.querySelector("#buttonAdd")
+buttonAdd.addEventListener('click', ()=>{
+    const inputItem = document.querySelector("#inputItem")
+    items.push(inputItem.value)
+
+    inputItem.value = ''
+    clearList()
+    listItem()
+    console.log(items)
+})
+function clearList(){
+    document.querySelector("#lista").innerHTML = ''
+}
+
+function removeItem(e){
+    let itemRemoving = e.target.parentNode
+    console.log(itemRemoving)
+}
+
